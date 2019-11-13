a1cd4231d2aaeca044cc67d72bb70f6114402a3f
Eray Erdin <eraygezer.94@gmail.com>
2019-11-14 02:10:11 +0300

    updated default secret key warning message

--- project/settings/base.py
+++ project/settings/base.py
@@ -14,14 +14,13 @@ __secret_key_env = os.environ.get("DJANGO_SECRET_KEY", None)
 if __secret_key_env:
     SECRET_KEY = __secret_key_env
 else:
-    __message = """
-    DJANGO_SECRET_KEY environment variable is not set.
-    Using secret key provided by Django on project
-    generation for debugging/development purposes.
-    Do not forget to set DJANGO_SECRET_KEY in
-    production environment.
-    """
-    __message = dedent(__message)
+    __message = (
+        "DJANGO_SECRET_KEY environment variable is not set. "
+        "Using secret key provided by Django on project generation for "
+        "debugging/development purposes. Do not forget to set DJANGO_SECRET_KEY "
+        "in production environment."
+    )
+    # __message = dedent(__message)
     warnings.warn(__message, RuntimeWarning)
 
 
