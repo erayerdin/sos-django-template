fe554273c92485058f94c1751fb0ff89cb044257
Eray Erdin <eraygezer.94@gmail.com>
2021-04-12 14:36:55 +0300

    added TimestampModel

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
-from django.db import models  # noqa
+from django.db import models
 
-# Create your models here.
+
+class TimestampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
